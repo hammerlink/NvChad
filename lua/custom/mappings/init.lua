caa4e7d11cc31663b3a47a32117ba1ab96023ae9
hendrik <hamerlinckhendrik@gmail.com>
2023-02-06 16:20:19 +0100

    add visual moving blocks (from the primeagen)

--- lua/custom/mappings/init.lua
+++ lua/custom/mappings/init.lua
@@ -1,10 +1,17 @@
 -- lua/custom/mappings
 local M = {}
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 M.undotree = {
   n = {
     ["<leader>tu"] = {"<cmd> UndotreeToggle <CR>", "Undotree toggle"}
-  }
+  },
+  v = {
+    ["J"] = {":m '>+1<CR>gv=gv"},
+    ["K"] = {":m '<-2<CR>gv=gv"},
+  },
 }
 
 return M
