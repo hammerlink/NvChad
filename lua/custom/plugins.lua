22602afdd49ddd7a9128b10c28ab2658f0ead8cf
hendrik <hendrik.hamerlinck@hammernet.be>
2024-06-05 08:42:04 +0200

    improve the neogit ? mapping

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -122,6 +122,12 @@ local plugins = {
                 integrations = {
                     diffview = true,
                 },
+                mappings = {
+                    popup = {
+                        ["g?"] = "HelpPopup",
+                        ["?"] = false,
+                    },
+                },
             }
         end,
     },
