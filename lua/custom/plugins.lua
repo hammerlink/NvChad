0e179f97a6d5d13d1f0eb33d112ab4c0c3d80734
hendrik <hendrik.hamerlinck@enbro.com>
2024-09-26 09:35:04 +0200

    remove trouble

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -312,16 +312,6 @@ local plugins = {
             }
         end,
     },
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        cmd = { "TroubleToggle", "TroubleRefresh", "Trouble", "TroubleClose" },
-    },
     ---- AI
     {
         "David-Kunz/gen.nvim",
