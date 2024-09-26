08e563342eb5126c77933bd5eeaa6e208daac852
hammerlink <hendrik.hamerlinck@hammernet.be>
2024-09-26 14:14:49 +0200

    properly implement q key binding for diffview

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -109,6 +109,20 @@ local plugins = {
     {
         "sindrets/diffview.nvim",
         event = "VeryLazy",
+        config = function()
+            local actions = require "diffview.actions"
+            require("diffview").setup {
+                keymaps = {
+                    view = { { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "close" } } },
+                    diff1 = { { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "close" } } },
+                    diff2 = { { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "close" } } },
+                    diff3 = { { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "close" } } },
+                    diff4 = { { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "close" } } },
+                    file_panel = { { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "close" } } },
+                    file_history_panel = { { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "close" } } },
+                },
+            }
+        end,
     },
     {
         "NeogitOrg/neogit",
