dc7693de7b0283db18247e32c96cfdf39d8a348a
hendrik <hendrik.hamerlinck@enbro.com>
2024-09-26 09:19:25 +0200

    improve the diffview highlighting

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -132,6 +132,12 @@ local plugins = {
             vim.cmd "highlight NeogitDiffDelete gui=bold guifg=#FAEBE8 guibg=#D1321B"
             vim.cmd "highlight NeogitDiffDeleteCursor gui=bold guifg=#D1321B guibg=#FAEBE8"
             vim.cmd "highlight NeogitDiffDeleteHighlight gui=bold guifg=#FAEBE8 guibg=#D1321B"
+
+            vim.cmd "highlight DiffviewDiffAddAsDelete guibg=#55433b guifg=NONE"
+            vim.cmd "highlight DiffviewDiffDelete guibg=#533649 guifg=NONE"
+            vim.cmd "highlight DiffviewDiffAdd guibg=#35533d guifg=NONE"
+            vim.cmd "highlight DiffviewDiffChange guibg=#29446c guifg=NONE"
+            vim.cmd "highlight DiffviewDiffText guibg=#3f426c guifg=NONE"
         end,
     },
     ------ Aerial ------
