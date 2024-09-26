b44486e5095aac37e8f731a21a22020b4ea3c962
hammerlink <hendrik.hamerlinck@hammernet.be>
2024-09-26 14:53:36 +0200

    add key binding to regularly open the file view history without the buffer

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -77,6 +77,7 @@ M.git = {
         ["<leader>gt"] = { "<cmd> Telescope git_status <CR>", "Git status" },
         ["<leader>gm"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
         ["<leader>gd"] = { "<cmd> DiffviewOpen <CR>", "Git Changes Diffview" },
+        ["<leader>gh"] = { "<cmd> DiffviewFileHistory<CR>", "Git FileHistory Diffview" },
         ["<leader>gs"] = { "<cmd> DiffviewFileHistory %<CR>", "Git FileHistory Diffview" },
         ["<leader>gr"] = { "<cmd> DiffviewRefresh <CR>", "Git FileHistory Diffview" },
         ["<leader>gx"] = { "<cmd> DiffviewClose <CR>", "Git Close Diffview" },
